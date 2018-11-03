7f307351eb6d0f9607a93a70417c0a2ca3057825
caitinggui <1029645297@qq.com>
2018-11-04 00:26:03 +0800

    move templates path

--- main.go
+++ main.go
@@ -12,13 +12,13 @@ func Health(c *gin.Context) {
 }
 
 func Hello(c *gin.Context) {
-	c.HTML(http.StatusOK, "hello.tmpl", gin.H{"title": "world"})
+	c.HTML(http.StatusOK, "blog/hello.tmpl", gin.H{"title": "world"})
 }
 
 func main() {
 	logger.Info("start cblog...")
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
+	router.LoadHTMLGlob("templates/**/*")
 	router.GET("/health", Health)
 	router.GET("/hello", Hello)
 	router.Run("0.0.0.0:8089")
